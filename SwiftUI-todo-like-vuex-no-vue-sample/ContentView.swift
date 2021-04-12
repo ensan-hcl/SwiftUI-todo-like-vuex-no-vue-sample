2e5ab8c0446f7d6a47eca78173932a5cc62b2756
En3_HCl <En3_HCl_K@icloud.com>
2021-04-12 22:04:20 +0900

    Initial Commit

---
+++ SwiftUI-todo-like-vuex-no-vue-sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-todo-like-vuex-no-vue-sample
+//
+//  Created by β α on 2021/04/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-todo-like-vuex-no-vue-sample/SwiftUI_todo_like_vuex_no_vue_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_todo_like_vuex_no_vue_sampleApp.swift
+//  SwiftUI-todo-like-vuex-no-vue-sample
+//
+//  Created by β α on 2021/04/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_todo_like_vuex_no_vue_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
